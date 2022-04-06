30f8a85925cb7a3357d5e411e90ee18804ee81af
mak9su4roi <makkusu.shiroi@gmail.com>
2022-04-06 19:11:01 +0300

    bank: Restructure project

--- py_pkg/__init__.py
+++
@@ -1,6 +0,0 @@
-from .config import DEPENDENCIES, TRIPLET, CONFIG_PRESETS, BUILD_PRESETS
-from .port import cmd_t_split, join_path
-from .cmds import FETCH_SUBMODULES_CMD, INIT_VCPKG_CMD, INSTALL_PKG_CMD_T, CMAKE_CONFIGURE_CMD_T, CMAKE_BUILD_CMD_T, \
-    KAFKA_CREATE_TOPICS_CMD_T, KAFKA_SYSTEMD_MODULE_ENABLE_CMD
-from .kafka_init import init_kafka
-
