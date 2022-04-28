ddb5b3083bb563817cd8440d9e52be2a225a4272
mak9su4roi <makkusu.shiroi@gmail.com>
2022-04-28 20:16:26 +0300

    bank: Fix mongo entrypoint script

--- docker/mongo-db/init.sh
+++ docker/mongo-db/init.sh
@@ -1,5 +1,14 @@
 #! bin/bash
 
-sleep 3 && mongosh -f /my-init/init.js && cat /my-init/init.js &
+inti_mongo() {
+    x=1
+    while [[ x -ne 0 ]]; do 
+        mongosh -f /my-init/init.js
+        x=${?}
+        sleep 0.5
+    done
+}
+
+inti_mongo &
 mongosh -f /my-init/init.js
 docker-entrypoint.sh "${@}"
