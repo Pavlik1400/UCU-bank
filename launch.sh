4427aa4d8c3e8fc735270aa9031f85380ad672ed
Pavlo Hilei <p.hilei@ucu.edu.ua>
2022-06-09 23:09:11 +0300

    Add launch.sh

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-kubectl create secret generic my-secret --from-file=credentials=/home/makkusu/.aws/credentials
+kubectl create secret generic my-secret --from-file=credentials=credentials
 kubectl apply -f kube.yaml
